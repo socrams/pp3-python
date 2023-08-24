29629149e7e77a3040abb6299287e5a61e0f0aa0
Nicolas Marcos <82241613+socrams@users.noreply.github.com>
2023-08-24 11:09:48 -0300

    .

--- config.py
+++ config.py
@@ -1,8 +1,21 @@
+# MOTOR_DB='mysql+pymysql'
+# URL_DB='localhost:3306'
+# NAME_DB='chat_server'
+# USER_DB='root'
+# PASS_DB='password'
+# TRACK_MODIFICATIONS=False
+# LANGUAJE='es_LA'
+# SIGNATURE_KEY='M+OQFg4DU/orix4gtZYV1aM8Rc8b3mmbCBn5B9SXFxs='
+
+POSTGRES_URL_NON_POOLING="postgres://default:IQKX0SzRq9jT@ep-purple-band-71611649.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+POSTGRES_HOST="ep-purple-band-71611649-pooler.us-east-1.postgres.vercel-storage.com"
+POSTGRES_DATABASE="verceldb"
+
 MOTOR_DB='mysql+pymysql'
-URL_DB='localhost:3306'
-NAME_DB='chat_server'
-USER_DB='root'
-PASS_DB='password'
+URL_DB="postgres://default:IQKX0SzRq9jT@ep-purple-band-71611649-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+NAME_DB="verceldb"
+USER_DB="default"
+PASS_DB="IQKX0SzRq9jT"
 TRACK_MODIFICATIONS=False
 LANGUAJE='es_LA'
 SIGNATURE_KEY='M+OQFg4DU/orix4gtZYV1aM8Rc8b3mmbCBn5B9SXFxs='
