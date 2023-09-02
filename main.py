e988d410bbca09d6297fc7d1b12706ea61f6219d
socrams <82241613+socrams@users.noreply.github.com>
2023-09-02 13:39:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from responses.routes import response_bp
 
 app = Flask(__name__)
 CORS(app)
-@cross_origin(allow_credentials=True)
 
 #incluimos los routes de cada metodos
 app.register_blueprint(careers_bp, url_prefix='/carrera')
@@ -26,6 +25,7 @@ app.register_blueprint(response_bp, url_prefix='/response')
 
 #interpreter=enchant.Dict(LANGUAJE)
 @app.route('/')
+@cross_origin(allow_credentials=True)
 def hello():
     return "¡Hola, esta es una API Flask!"
 
